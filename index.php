d1281008761053c5f2978d2f287cfe79c9f22ca3
Joel Perpetua <qwerty.perpetua@gmail.com>
2020-01-21 20:38:13 -0300

    Added CV pages for each langauge so Linkedin can get the OG for each one of the files

--- index.php
+++ index.php
@@ -8,7 +8,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+
+    <!-- og -->
+    <meta property="og:title" content="My portfolio">
+    <meta property="og:image" content="">
+    <meta property="og:description" content="This is my portfolio">
+    <meta property="og:url" content="https://joeperpetua.me/">
+
+    <title>Joel Perpetua - Portfolio</title>
 </head>
 <body>
     <script>
