43c2ccc46780b0c1a22866398efc2c98bcb46a42
Joel Perpetua <qwerty.perpetua@gmail.com>
2020-02-04 10:51:35 -0300

    Added meta info and added OG thumbnail

--- index.php
+++ index.php
@@ -5,20 +5,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- meta -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="google-site-verification" content="M4IhjQxwpOjAYYRJwrCIKl3H71WfME13z5ovbgaGams" />
+
+    <title>Joel Perpetua - Portfolio</title>
+    <meta name="description" content="Hi, I am Joel and this is my online portfolio, here you will find all my projects and skills.">
+    <meta name="author" content="Joel Perpetua">
+    <meta name="keywords" content="Joel,Perpetua,Joel Perpetua,Portfolio,Web developper,Resume,CV,Software Developper,E.E.S.T N7">
+
+    <link rel="icon" type="image/png" href="../img/icon/user.png" sizes="32x32">
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-156693045-1"></script>
     <script src="js/gtag.js"></script>
-
+    <meta name="google-site-verification" content="M4IhjQxwpOjAYYRJwrCIKl3H71WfME13z5ovbgaGams" />
 
     <!-- og -->
-    <meta property="og:title" content="My portfolio">
-    <meta property="og:image" content="">
-    <meta property="og:description" content="This is my portfolio">
+    <meta property="og:title" content="Joel Perpetua - Portfolio">
+    <meta property="og:image" content="https://joeperpetua.me/img/thumbnail-en.png">
+    <meta property="og:description" content="Hi, I am Joel and this is my online portfolio, here you will find all my projects and skills.">
     <meta property="og:url" content="https://joeperpetua.me/">
 
     <title>Joel Perpetua - Portfolio</title>
